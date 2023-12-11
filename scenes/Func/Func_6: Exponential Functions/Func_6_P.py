f78d49fe868c35b679321a0de0a4157727b9d0c3
Friedrich Wicke <friedrich.wicke@protonmail.com>
2023-12-12 00:40:39 +0100

    updates

--- scenes/Func/Func_6: Exponential Functions/Func_6_P.py
+++ scenes/Func/Func_6: Exponential Functions/Func_6_P.py
@@ -314,7 +314,7 @@ class Func_6_P_recognize_growth_1_d(SophiaCursorScene):
             self.play(Write(f_2), f_1.animate.set_color(GREY), f_3.animate.set_color(GREY))
 
         self.wait(4)
-        gbv 
+
 
 #####################################
 #####################################
@@ -4683,6 +4683,53 @@ class Func_6_P_2_2_3_d(SophiaCursorScene):
 
 
 PROTOTYPES=[
+########################################################################## Practice: Recognizing Exponential Growth
+    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_1_q),
+    PagePrototypeQuestion.from_scene(Func_6_P_recognize_growth_1_q),
+    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_1_a),
+    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_1_b),
+    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_1_c),
+    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_1_d),
+    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_2_q),
+    PagePrototypeQuestion.from_scene(Func_6_P_recognize_growth_2_q),
+    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_2_a),
+    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_2_b),
+    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_2_c),
+    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_2_d),
+    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_3_q),
+    PagePrototypeQuestion.from_scene(Func_6_P_recognize_growth_3_q),
+    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_3_a),
+    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_3_b),
+    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_3_c),
+    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_3_d),
+########################################################################## Practice: Growth Factor <-> Graph
+    PagePrototypeVideo.from_scene(Func_6_2_P_2_q),
+    PagePrototypeQuestion.from_scene(Func_6_2_P_2_q),
+    PagePrototypeVideo.from_scene(Func_6_2_P_2_a),
+    PagePrototypeVideo.from_scene(Func_6_2_P_2_b),
+    PagePrototypeVideo.from_scene(Func_6_2_P_2_c),
+    PagePrototypeVideo.from_scene(Func_6_2_P_3_q),
+    PagePrototypeQuestion.from_scene(Func_6_2_P_3_q),
+    PagePrototypeVideo.from_scene(Func_6_2_P_3_a),
+    PagePrototypeVideo.from_scene(Func_6_2_P_3_b),
+    PagePrototypeVideo.from_scene(Func_6_2_P_3_c),
+    PagePrototypeVideo.from_scene(Func_6_2_P_3_d),
+    PagePrototypeVideo.from_scene(Func_6_2_P_4_q),
+    PagePrototypeQuestion.from_scene(Func_6_2_P_4_q),
+    PagePrototypeVideo.from_scene(Func_6_2_P_4_a),
+    PagePrototypeVideo.from_scene(Func_6_2_P_4_b),
+    PagePrototypeVideo.from_scene(Func_6_2_P_4_c),
+    PagePrototypeVideo.from_scene(Func_6_2_P_5_q),
+    PagePrototypeQuestion.from_scene(Func_6_2_P_5_q),
+    PagePrototypeVideo.from_scene(Func_6_2_P_5_a),
+    PagePrototypeVideo.from_scene(Func_6_2_P_5_b),
+    PagePrototypeVideo.from_scene(Func_6_2_P_5_c),
+    PagePrototypeVideo.from_scene(Func_6_2_P_5_d),
+    PagePrototypeVideo.from_scene(Func_6_2_P_6_q),
+    PagePrototypeQuestion.from_scene(Func_6_2_P_6_q),
+    PagePrototypeVideo.from_scene(Func_6_2_P_6_a),
+    PagePrototypeVideo.from_scene(Func_6_2_P_6_b),
+    PagePrototypeVideo.from_scene(Func_6_2_P_6_c),
 ##########################################################################CHAPTER: Rules for Exponenial Functions: Easy
     PagePrototypeVideo.from_scene(Func_6_P_2_1_1_q),
     PagePrototypeQuestion.from_scene(Func_6_P_2_1_1_q),
@@ -4727,138 +4774,4 @@ PROTOTYPES=[
     PagePrototypeVideo.from_scene(Func_6_P_2_2_3_b),
     PagePrototypeVideo.from_scene(Func_6_P_2_2_3_c),
     PagePrototypeVideo.from_scene(Func_6_P_2_2_3_d),
-]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# #####################################
-# # Exercise 1: On Money (compare Aufgabe 5, Manuel Benz)
-# class Func_6_4_I_1_1_q(SophiaCursorScene):
-
-#     # def task_definition(self) -> SophiaTaskDefinition:
-#     #     return SophiaTaskDefinition(
-#     #         answerOptions = ["$x=\sqrt[5]{625}$", "$x=\sqrt[625]{5}$", "$x=\log_{625}{5}$", "$x=\log_5{625}$"],
-#     #         correctAnswerIndex = 3,
-#     #         questionText=self.translate("Func_6_3.I4.q.question-text")
-#     #     )
-
-#     # Main method for constructing the animation
-#     def construct(self):
-#         # Adding initial components to the scene
-#         super().construct()
-#         self.add_mathgrid()
-
-#         def cursor_sound_updater(mob, dt):
-#             if mob.needSound:
-#                 mob.needSound = False
-#                 self.add_cursor_sound()
-#         x,y,_ = 0,0,0
-#         cursor = AltCursor(stroke_width=0.0, blinking=True, x=x, y=y)
-#         cursor.autoFadeBackground = True
-#         cursor.add_updater(cursor_sound_updater)
-#         self.add(cursor)
-
-#         capital = self.translate("words.capital")
-#         interest = self.translate("words.interest")
-#         years = self.translate("words.years")
-#         compound_interest = self.translate("words.compound_interest")
-#         bullets = VGroup(Tex(f"$\\bullet$ {capital}: 500\\$", color=c1t, font_size=fs2), Tex(f"$\\bullet$ {interest}: 10\\%", color=c1t, font_size=fs2), Tex("$\\bullet$ $10$ {years}", color=c1t, font_size=fs2))
-#         title = Tex(compound_interest, color=c1t, font_size=fs2).next_to(bullets, UP, buff=0.6)
-#         # Action Sequence
-#         with self.voiceover(
-#                 text="""
-# In the realm of finance and investment, a common question arises: what is the future value of an investment under<bookmark mark="compound_in"/> compound interest conditions? For instance, consider the following scenario: An initial capital <bookmark mark="capital_in"/>of 500 Dollars is invested at an interest rate <bookmark mark="interest_in"/>of 5 percent. The interest is compounded annually. What would be the total amount of this capital after a <bookmark mark="time_in"/>period of 10 years?
-# """
-#         ) as tracker:
-            
-#             self.wait_until_bookmark("compound_in")
-#             self.play(Write(title))
-
-#             self.wait_until_bookmark("capital_in")
-#             x,y,_ = bullets[0].get_center()+0.4*DOWN
-#             cursor.idle=False
-#             self.play(CursorMoveTo(cursor, x,y), Write(bullets[0]), run_time=.5)
-
-#             self.wait_until_bookmark("interest_in")
-#             x,y,_ = bullets[1].get_center()+0.4*DOWN
-#             self.play(CursorMoveTo(cursor, x,y), Write(bullets[1]), run_time=.5)
-
-#             self.wait_until_bookmark("time_in")
-#             x,y,_ = bullets[2].get_center()+0.4*DOWN
-#             self.play(CursorMoveTo(cursor, x,y), Write(bullets[2]), run_time=.5)
-#             cursor.idle=True
-
-#         self.wait(4)
-
-
-PROTOTYPES=[
-########################################################################## Practice: Recognizing Exponential Growth
-    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_1_q),
-    PagePrototypeQuestion.from_scene(Func_6_P_recognize_growth_1_q),
-    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_1_a),
-    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_1_b),
-    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_1_c),
-    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_1_d),
-    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_2_q),
-    PagePrototypeQuestion.from_scene(Func_6_P_recognize_growth_2_q),
-    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_2_a),
-    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_2_b),
-    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_2_c),
-    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_2_d),
-    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_3_q),
-    PagePrototypeQuestion.from_scene(Func_6_P_recognize_growth_3_q),
-    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_3_a),
-    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_3_b),
-    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_3_c),
-    PagePrototypeVideo.from_scene(Func_6_P_recognize_growth_3_d),
-########################################################################## Practice: Growth Factor <-> Graph
-    # PagePrototypeVideo.from_scene(Func_6_2_P_1_q),
-    # PagePrototypeQuestion.from_scene(Func_6_2_P_1_q),
-    # PagePrototypeVideo.from_scene(Func_6_2_P_1_a),
-    # PagePrototypeVideo.from_scene(Func_6_2_P_1_b),
-    # PagePrototypeVideo.from_scene(Func_6_2_P_1_c),
-    # PagePrototypeVideo.from_scene(Func_6_2_P_1_d), -> Used in instruction chapter
-    PagePrototypeVideo.from_scene(Func_6_2_P_2_q),
-    PagePrototypeQuestion.from_scene(Func_6_2_P_2_q),
-    PagePrototypeVideo.from_scene(Func_6_2_P_2_a),
-    PagePrototypeVideo.from_scene(Func_6_2_P_2_b),
-    PagePrototypeVideo.from_scene(Func_6_2_P_2_c),
-    PagePrototypeVideo.from_scene(Func_6_2_P_3_q),
-    PagePrototypeQuestion.from_scene(Func_6_2_P_3_q),
-    PagePrototypeVideo.from_scene(Func_6_2_P_3_a),
-    PagePrototypeVideo.from_scene(Func_6_2_P_3_b),
-    PagePrototypeVideo.from_scene(Func_6_2_P_3_c),
-    PagePrototypeVideo.from_scene(Func_6_2_P_3_d),
-    PagePrototypeVideo.from_scene(Func_6_2_P_4_q),
-    PagePrototypeQuestion.from_scene(Func_6_2_P_4_q),
-    PagePrototypeVideo.from_scene(Func_6_2_P_4_a),
-    PagePrototypeVideo.from_scene(Func_6_2_P_4_b),
-    PagePrototypeVideo.from_scene(Func_6_2_P_4_c),
-    PagePrototypeVideo.from_scene(Func_6_2_P_5_q),
-    PagePrototypeQuestion.from_scene(Func_6_2_P_5_q),
-    PagePrototypeVideo.from_scene(Func_6_2_P_5_a),
-    PagePrototypeVideo.from_scene(Func_6_2_P_5_b),
-    PagePrototypeVideo.from_scene(Func_6_2_P_5_c),
-    PagePrototypeVideo.from_scene(Func_6_2_P_5_d),
-    PagePrototypeVideo.from_scene(Func_6_2_P_6_q),
-    PagePrototypeQuestion.from_scene(Func_6_2_P_6_q),
-    PagePrototypeVideo.from_scene(Func_6_2_P_6_a),
-    PagePrototypeVideo.from_scene(Func_6_2_P_6_b),
-    PagePrototypeVideo.from_scene(Func_6_2_P_6_c),
 ]
