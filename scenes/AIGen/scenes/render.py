1bdc6ea1be7f769fa412d7f578d8a12c01bc997e
Friedrich Wicke <friedrich.wicke@protonmail.com>
2024-03-03 00:33:37 +0100

    add prototype gen

--- scenes/AIGen/scenes/render.py
+++ scenes/AIGen/scenes/render.py
@@ -4,21 +4,9 @@ from sophialib.morphing.sophiamorphscene import AutoSlideScene
 
 class ExampleScene(AutoSlideScene):
     def construct(self):
-        # parse the corresponding typst
-
-        filename = "1_integral_02.py"
-        self.parse_corresponding_typst_scene(
-            Path(__file__).parent / filename
-        )
-        
-
-        # Adding initial components to the scene
+        filename = "1_integral_04.py"
+        self.parse_corresponding_typst_scene(Path(__file__).parent / filename)
         super().construct()
-        
-        # start auto slide show
-        # self.auto_slide()
-
         self.auto_slide_with_voiceover()
 
 
-    
