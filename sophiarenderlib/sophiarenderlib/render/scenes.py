9d46bbdaa68574ffc97b9ada5f0e24cdde8ab8d3
Gero <gero.embser@gmail.com>
2024-05-11 19:26:17 +0200

    refactor

--- sophiarenderlib/sophiarenderlib/render/scenes.py
+++ sophiarenderlib/sophiarenderlib/render/scenes.py
@@ -12,15 +12,20 @@ import multiprocessing as mp
 from sophialib.styles.sophiascene import SophiaScene
 from sophialib.videopostprocessing.crop import crop_video_file
 
+# A method that will create the output file name given a scene class and a locale
+def create_output_file_name(sc: Type[SophiaScene], parent_output_dir: Path, include_locale_in_file: bool = True):
+    elevenlabs_voice_name = sc().current_elevenlabs_voice()
+    locale_prefix = f"{sc().current_locale().upper()}{'['+elevenlabs_voice_name+']' if elevenlabs_voice_name is not None else ''}_" if include_locale_in_file else ""
+    output_path = (parent_output_dir/sc.__module__/f"{locale_prefix}{sc.__name__}.mp4").resolve()
+
+    return output_path
 
 
 def render_scene(sc: Type[SophiaScene], output_dir: Path, scene_media_folder: Path, test_run_with_last_frame: bool = False, disable_progressbar: bool = False, quality: str = "production_quality", include_locale_in_file: bool = True):
     if test_run_with_last_frame:
         output_path = (output_dir/sc.__module__/f"{sc.__name__}_lastframe.png").resolve()
     else: 
-        elevenlabs_voice_name = sc().current_elevenlabs_voice()
-        locale_prefix = f"{sc().current_locale().upper()}{'['+elevenlabs_voice_name+']' if elevenlabs_voice_name is not None else ''}_" if include_locale_in_file else ""
-        output_path = (output_dir/sc.__module__/f"{locale_prefix}{sc.__name__}.mp4").resolve()
+        output_path = create_output_file_name(sc, output_dir, include_locale_in_file)
     output_path.parent.mkdir(parents=True, exist_ok=True) #make sure always exist
     config = {"quality": quality, "preview": False, "output_file": str(output_path), "disable_caching": True, "media_dir": scene_media_folder} # disable caching is important for manim-voiceover (due to a bug in manim)
     if test_run_with_last_frame:
